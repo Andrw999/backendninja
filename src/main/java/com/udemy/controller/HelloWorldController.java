73a15a5f46691a6312605b97e12a865f7ca247c2
Andrw999 <akracia99@gmail.com>
2017-01-18 13:29:19 -0600

    Project files

---
+++ src/main/java/com/udemy/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping( "/say" )
+public class HelloWorldController {
+
+	@GetMapping( "/helloworld" )
+	public String helloWorld( ){
+		return "helloworld";
+	}
+	
+}
