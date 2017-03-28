7728c0d6e476983c66d7373bc5fc3debfcfbd3e3
Andrw999 <akracia99@gmail.com>
2017-03-28 13:00:50 -0600

    Persistence. Section 6, Lecture 40.

---
+++ src/main/java/com/udemy/entity/Course.java
@@ -0,0 +1,78 @@
+package com.udemy.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+	@Id
+	@GeneratedValue
+	@Column( name="id" )
+	private int id;
+	
+	private String name;
+	
+	private String description;
+	
+	private double price;
+	
+	private int hours;
+
+	public Course() {
+	}
+
+	public Course(int id, String name, String description, double price, int hours) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.hours = hours;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+}
